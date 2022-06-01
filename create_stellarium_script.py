bce07a8d77216dd59c691878957cc6690896bd7f
Evgeny Arbatov <arbatovevgeny@gmail.com>
2022-06-01 02:00:28 +0000

    Tried sampling but azimuth is still off

--- create_stellarium_script.py
+++ create_stellarium_script.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+import random
 import gpxpy
 import pyproj
 import sys	
 from dateutil import tz
 
 LOCAL_TZ = 'Asia/Singapore'
+SAMPLE_SIZE = 100
 
 # python3 create_stellarium_script.py data/Morning_Run.gpx 
 
@@ -25,25 +27,27 @@ def parse_gpx_file(filename):
 			for point in segment.points:
 				points.append((
 					get_local_time(point.time), 
-					point.latitude, 
 					point.longitude, 
+					point.latitude, 
 					point.elevation					
 				))
 
-	return points
+	sampled_points = random.sample([x for x in points], SAMPLE_SIZE)
+
+	return sampled_points
 
 def get_bearing(points):
 	for idx, point in enumerate(points, start=1):
-		geodesic = pyproj.Geod(ellps='WGS84')
-
 		previous_point = points[idx-1]
 
-		fwd_azimuth, back_azimuth, distance = geodesic.inv(
-			previous_point[2],
-			previous_point[1],
-			point[2], 
-			point[1]
-		)
+		G = pyproj.Geod(ellps='WGS84')
+
+		fwd_azimuth = G.inv(
+			float(previous_point[1]),
+			float(previous_point[2]),
+			float(point[1]),
+			float(point[2])
+		)[0]
 
 		print(
 			point[0],
@@ -57,7 +61,6 @@ def main(args):
 	gpx_file = args[0]
 
 	points = parse_gpx_file(gpx_file)
-
 	get_bearing(points)
 
 if __name__ == "__main__":
