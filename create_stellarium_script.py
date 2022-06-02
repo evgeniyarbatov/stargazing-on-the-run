deda52337d4482cf7da119b94aef9345ca83063e
Evgeny Arbatov <arbatovevgeny@gmail.com>
2022-06-02 15:50:43 +0000

    Started adding the script

--- create_stellarium_script.py
+++ create_stellarium_script.py
@@ -11,22 +11,72 @@ LOCAL_TZ = 'Asia/Singapore'
 # python3 create_stellarium_script.py data/Morning_Run.gpx 
 
 class StellariumScript:
-	    __script = """
+	__args = None
+	__script = """
     // Author: Evgeny Arbatov
     // Version: 1.0
     // License: Public Domain
     // Name: GPX to Stellarium images
     // Description: Convert GPX files to Stellarium images
 
-	param_frame_folder = "$FRAME_FOLDER$"
     param_az = $AZ$
     param_alt = $ALT$
     param_lat = $LAT$
     param_long = $LONG$
-    param_title = "$TITLE$"
     param_date = "$DATE$"
 
+    core.setTimeRate(0); 
+    core.setGuiVisible(false);
+    core.setMilkyWayVisible(true);
+    core.setMilkyWayIntensity(4);
+    SolarSystem.setFlagPlanets(true);
+    SolarSystem.setMoonScale(6);
+    SolarSystem.setFlagMoonScale(true);
+    SolarSystem.setFontSize(25);
+    
+    StelSkyDrawer.setAbsoluteStarScale(1.5);
+    StelSkyDrawer.setRelativeStarScale(1.65);
+    StarMgr.setFontSize(20);
+    StarMgr.setLabelsAmount(3);
+    ConstellationMgr.setFlagLines(true);
+    ConstellationMgr.setFlagLabels(true);
+    ConstellationMgr.setArtIntensity(0.1);
+    ConstellationMgr.setFlagArt(true);
+    ConstellationMgr.setFlagBoundaries(false);
+    ConstellationMgr.setConstellationLineThickness(3);
+    ConstellationMgr.setFontSize(18);
+
+    LandscapeMgr.setFlagAtmosphere(true);
+    StelMovementMgr.zoomTo(70, 0);
+    core.wait(0.5);
+
+	core.setDate(date, "utc");
+	core.setObserverLocation(long, lat, 0, 0, "Singapore", "Earth");
+	core.wait(0.5);
+	core.moveToAltAzi(alt, azi)
+	core.wait(0.5);q
+
+	core.setDate('+' + param_dt + ' seconds');
+	core.screenshot(file_prefix);
+
+    core.setGuiVisible(true);
+    core.quitStellarium();
 	"""
+
+    def __init__(self, args):
+        self.__args = args
+
+	def create_script(self):
+		script = self.__script
+        script = script.replace("$LAT$", self.__args['lat'])
+        script = script.replace("$LONG$", self.__args['long'])
+        script = script.replace("$DATE$", self.__args['date'])
+        script = script.replace("$AZ$", self.__args['az'])
+        script = script.replace("$ALT$", self.__args['alt'])
+
+		file = open("script.ssc", "w")
+        file.write(script)
+        file.close()
 
 def get_local_time(dt):
 	local_tz = tz.gettz(LOCAL_TZ)
@@ -84,6 +134,11 @@ def main(args):
 
 	points = parse_gpx_file(gpx_file)
 	points_with_bearing = get_bearing(points)
+
+	script = StellariumScript(dict[
+
+	])
+    script.create_script();
 
 if __name__ == "__main__":
     main(sys.argv[1:])
