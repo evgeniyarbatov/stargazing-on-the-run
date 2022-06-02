66415ef0f4c9f0af3a01f22853f7067d70deae5e
Evgeny Arbatov <arbatovevgeny@gmail.com>
2022-06-02 01:55:34 +0000

    Started adding the script

--- create_stellarium_script.py
+++ create_stellarium_script.py
@@ -10,6 +10,24 @@ LOCAL_TZ = 'Asia/Singapore'
 
 # python3 create_stellarium_script.py data/Morning_Run.gpx 
 
+class StellariumScript:
+	    __script = """
+    // Author: Evgeny Arbatov
+    // Version: 1.0
+    // License: Public Domain
+    // Name: GPX to Stellarium images
+    // Description: Convert GPX files to Stellarium images
+
+	param_frame_folder = "$FRAME_FOLDER$"
+    param_az = $AZ$
+    param_alt = $ALT$
+    param_lat = $LAT$
+    param_long = $LONG$
+    param_title = "$TITLE$"
+    param_date = "$DATE$"
+
+	"""
+
 def get_local_time(dt):
 	local_tz = tz.gettz(LOCAL_TZ)
 	dt = dt.replace(tzinfo=tz.gettz('UTC'))
@@ -34,6 +52,8 @@ def parse_gpx_file(filename):
 	return points
 
 def get_bearing(points):
+	points_with_bearing = []
+
 	for idx, point in enumerate(points, start=1):
 		# We need to offset since points are very close
 		previous_point = points[
@@ -49,19 +69,21 @@ def get_bearing(points):
 			float(point[2])
 		)[0]
 
-		print(
+		points_with_bearing.append((
 			point[0],
 			point[1],
 			point[2],
 			point[3],
 			fwd_azimuth
-		)
+		))
+
+	return points_with_bearing
 
 def main(args):
 	gpx_file = args[0]
 
 	points = parse_gpx_file(gpx_file)
-	get_bearing(points)
+	points_with_bearing = get_bearing(points)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
