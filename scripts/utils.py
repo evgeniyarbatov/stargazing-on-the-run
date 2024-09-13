4700c07b9ac6163435e7c1b015343bd303c113a9
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-09-13 12:33:12 +0700

    Fix azimuth reading

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,8 @@
 import gpxpy
 import pytz
 
+import xml.etree.ElementTree as ET
+
 class Point:
 	def __init__(
 		self, 
@@ -34,6 +36,8 @@ class GPXData:
    
 		gpx.simplify()
 
+		compass_data = self.get_compass(filename)
+
 		for track in gpx.tracks:
 			for segment in track.segments:
 				for point in segment.points:
@@ -44,15 +48,43 @@ class GPXData:
 						pytz.timezone(timezone)
 					)
 
+					az = self.get_compass_reading(
+						compass_data, 
+						point.time,
+					)
+
 					self.points.append(
 						Point(
 							local_time.strftime('%Y-%m-%dT%H:%M:%S'), 
 							local_time.strftime('%s'), 
 							point.latitude, 
 							point.longitude,
-							"0",
+							az,
 						)
 					)
+
+	def get_compass(self, filename):
+		compass_data = {}
+     
+		root = ET.parse(filename).getroot()
+		for trk in root.findall('.//{http://www.topografix.com/GPX/1/1}trk'):
+			for trkseg in trk.findall('{http://www.topografix.com/GPX/1/1}trkseg'):
+				for trkpt in trkseg.findall('{http://www.topografix.com/GPX/1/1}trkpt'):
+					time = trkpt.find('{http://www.topografix.com/GPX/1/1}time')
+					time = time.text if time is not None else None
+					
+					extensions = trkpt.find('{http://www.topografix.com/GPX/1/1}extensions')
+					if extensions is not None:
+						compass = extensions.find('{http://www.topografix.com/GPX/1/1}compass')
+						compass = float(compass.text) if compass is not None else None
+
+						compass_data[time] = float(compass)
+						
+		return compass_data
+
+	def get_compass_reading(self, compass_data, time):
+		time = time.strftime('%Y-%m-%dT%H:%M:%SZ')
+		return compass_data[time]
 
 	def get_points(self):
 		return self.points	
